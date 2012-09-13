9dcf79916dc82fb04189cbe60408b04096408477
marcelo oliveira <marcelo_o@hotmail.com>
2012-09-12 23:17:48 -0300

    first version

---
+++ Feedback360.Web/Scripts/feedback.js
@@ -0,0 +1,77 @@
+﻿var Song = Backbone.Model.extend({
+    defaults: {
+        name: "Not specified",
+        artist: "Not specified"
+    },
+    initialize: function () {
+        console.log("Music is the answer");
+    }
+});
+
+var Album = Backbone.Collection.extend({
+    model: Song
+});
+
+var song1 = new Song({ name: "How Bizarre", artist: "OMC" });
+var song2 = new Song({ name: "Sexual Healing", artist: "Marvin Gaye" });
+var song3 = new Song({ name: "Talk It Over In Bed", artist: "OMC" });
+
+var myAlbum = new Album([song1, song2, song3]);
+
+SimpleSample = {};
+
+SimpleSample.Person = Backbone.Model.extend({});
+
+SimpleSample.Participants = Backbone.Model.extend({});
+
+SimpleSample.FormView = Backbone.View.extend({
+    el: "#edit-form",
+    
+    initialize: function(){
+        Backbone.ModelBinding.call(this);
+    }
+});
+
+SimpleSample.DisplayView = Backbone.View.extend({
+    el: "#display",
+    
+    events: {
+        "click #reset-button": "reset"
+    },
+    
+    initialize: function(){
+        Backbone.ModelBinding.call(this);
+    },
+        
+    reset: function(){
+        this.model.set({name: 'Marty McFly', position: 'manager', first_name: "joe", last_name: "bob"});
+    }
+});
+
+SimpleSample.ParticipantsGridView = Backbone.View.extend({
+    el: "#participants-grid",
+
+    initialize: function () {
+        Backbone.ModelBinding.call(this);
+    },
+
+    reset: function () {
+        this.model.set(myAlbum);
+    }
+});
+
+SimpleSample.App = function(){
+    this.start = function(){
+        var person = new SimpleSample.Person();
+        var formView = new SimpleSample.FormView({model: person});
+        var displayView = new SimpleSample.DisplayView({ model: person });
+        var participantsGridView = new SimpleSample.ParticipantsGridView({ model: myAlbum });
+
+        person.set({ name: 'Marty McFly', position: 'manager', first_name: "joe", last_name: "bob" });
+    }
+}
+    
+$(function(){
+    var app = new SimpleSample.App();
+    app.start();
+})
---
+++ Feedback360.Web/Views/Feedback.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src="../Scripts/jquery-1.7.2.min.js" type="text/javascript"></script>
+    <script src="../Scripts/underscore.min.js" type="text/javascript"></script>
+    <script src="../Scripts/backbone.min.js" type="text/javascript"></script>
+    <script src="../Scripts/backbone.modelbinding.js" type="text/javascript"></script>
+    <script src="../Scripts/feedback.js" type="text/javascript"></script>
+    <link href="../Styles/feedback.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <div class="central-panel">
+        <div class="title">
+            <img src="../Images/Logo.png" />
+        </div>
+        <div class="panel">
+            <form id="edit-form">
+                first name:
+                <input id="first_name" type="text"><br />
+                last name:
+                <input id="last_name" type="text"><br />
+            </form>
+            <br />
+
+            <div id="participants-grid" class="participants-grid">
+                <div><span data-bind="text name"></span><span data-bind="text artist"></span></div>
+            </div>
+            <br />
+
+            <div id="display" class="panel">
+                You Entered <span data-bind="text first_name"></span><span data-bind="text last_name">
+                </span>
+                <br />
+                <input type="button" class="button" id="reset-button" value="Reset The Form"></button>
+            </div>
+        </div>
+    </div>    ​
+</body>
+</html>
